f75cf678efb96693a0a572f71d4d7f270f08cdfe
Liron Aravot <laravot@redhat.com>
2016-04-12 18:47:38 +0300

    adding support for local commits
    This change adds support to use the helper for local commits and
    not only on topics.
    As the util no longer supports topics only, its renamed to gerrit-helper
    instead of gerrit-topic-helper.

--- gerrit-helper.py
+++ gerrit-helper.py
@@ -3,30 +3,35 @@ import json
 import argparse
 import logging
 import sys
+import subprocess
 
-parser = argparse.ArgumentParser(description='Gerrit topics helper')
+parser = argparse.ArgumentParser(description='Gerrit helper')
 parser.add_argument('-r', '--reviewers', nargs='+', help='specifies the reviewers to add to the patches')
 parser.add_argument('-v', '--verbose', action='store_true', help='execute with extensive logging')
-parser.add_argument('-cr', '--code-review', dest='codeReview', help='code review to apply')
-parser.add_argument('-vr', '--verified', dest='verified', help='verified review to apply')
-parser.add_argument('-lb', '--local-branch', dest='localBranch', help='perform the operation on a local branch')
+parser.add_argument('-cr', '--code-review', dest='codeReview', help='code review score to apply')
+parser.add_argument('-vr', '--verified', dest='verified', help='verified score to apply')
+parser.add_argument('-a', '--abandon',  dest='abandon', help='abandon the topic patches')
+parser.add_argument('-d', '--delete-drafts',  dest='deleteDrafts', help='delete draft changes')
 parser.set_defaults(abandon=False)
-subparsers = parser.add_subparsers(help='sub-command help')
+subparsers = parser.add_subparsers(help='Operation options', dest='subparser_name')
 
-gerrit_topic_parser = subparsers.add_parser('gerrit-topic', help='Gerrit topics')
+gerrit_topic_parser = subparsers.add_parser('gerrit-topic', help='Gerrit patches topics')
 gerrit_topic_parser.add_argument('-t','--topics', nargs='+', help='topics to perform on', required=True)
 gerrit_topic_parser.add_argument('-o', '--owners', nargs='+', help='specifies the owners of the patches', required=True)
 gerrit_topic_parser.add_argument('-pr', '--projects', nargs='+', help='specifies the projects', required=True)
-gerrit_topic_parser.add_argument('-a', '--abandon',  dest='abandon', help='abandon the topic patches')
-gerrit_topic_parser.add_argument('-d', '--delete-drafts',  dest='deleteDrafts', help='delete draft changes')
 
 current_branch_parser = subparsers.add_parser('current-branch', help='Current branch')
-gerrit_topic_parser.add_argument('-p', '--path',  dest='path', help='path', required=True)
+current_branch_parser.add_argument('-p', '--path',  dest='path', help='path', required=True)
+current_branch_parser.add_argument('-fc', '--first-commit',  dest='firstCommit', help='first commit to perform on, '
+																					  'inclusive', required=True)
+current_branch_parser.add_argument('-lc', '--last-commit',  dest='lastCommit', help='last commit to perform on, '
+																					'exclusive', required=True)
+
 
 args = parser.parse_args()
 d = vars(args)
 
-log = logging.getLogger('Gerrit-topic-helper')
+log = logging.getLogger('Gerrit helper')
 log.setLevel(logging.INFO)
 sl = logging.StreamHandler(sys.stdout)
 sl.setLevel(logging.DEBUG)
@@ -71,12 +76,6 @@ def get_topic_changes(d):
 	result = query_result.stdout.readlines()
 	return result[:-1]
 
-data = get_topic_changes(d)
-
-reviewers = safeGet("reviewers", d)
-code_review = safeGet("codeReview", d)
-verified = safeGet("verified", d)
-
 def get_topic_commits(data):
 	result = []
 	for line in data:
@@ -86,18 +85,23 @@ def get_topic_commits(data):
 
 	return result
 
-def get_branch_commits(data):
-	command = 'git log --format="%H"'
-	subprocess.run(["git log ", "-l", "/dev/null"], stdout=subprocess.PIPE)
-	result = []
-	for line in data:
-		parsed=json.loads(line)
-		commit=parsed["currentPatchSet"]["revision"]
-		result += [commit]
-
+def get_branch_commits(path, first_commit, last_commit):
+	command = "git --git-dir {}/.git rev-list {} ^{}".format(path, first_commit, last_commit)
+	res = subprocess.check_output([command], shell=True)
+	result = res.splitlines()
 	return result
 
-commits = get_topic_commits(data)
+reviewers = safeGet("reviewers", d)
+code_review = safeGet("codeReview", d)
+verified = safeGet("verified", d)
+
+if d["subparser_name"] == 'current-branch':
+	commits = get_branch_commits(d["path"], d["firstCommit"], d["lastCommit"])
+else:
+	data = get_topic_changes(d)
+	commits = get_topic_commits(data)
+
+log.info(commits)
 
 for commit in commits:
 	if reviewers is not None:
@@ -126,5 +130,5 @@ for commit in commits:
 		if abandon:
 			log.debug("abandoning %s", commit)
 			review_com+=" --abandon {}".format(commit)
-	log.debug("running git review command %s", review_com)
+	log.debug("running command %s", review_com)
 	client.run_gerrit_command(review_com)
