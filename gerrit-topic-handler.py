7981dda4dd23c3ef499505c19abff0e621bbc533
Liron Aravot <laravot@redhat.com>
2016-04-12 15:04:44 +0300

    tasd

--- gerrit-topic-handler.py
+++ gerrit-topic-handler.py
@@ -5,16 +5,23 @@ import logging
 import sys
 
 parser = argparse.ArgumentParser(description='Gerrit topics helper')
-parser.add_argument('-t','--topics', nargs='+', help='topics to perform on', required=True)
-parser.add_argument('-d', '--delete-drafts',  dest='deleteDrafts', help='delete draft changes')
-parser.add_argument('-a', '--abandon',  dest='abandon', help='abandon the topic patches')
-parser.add_argument('-o', '--owners', nargs='+', help='specifies the owners of the patches', required=True)
-parser.add_argument('-p', '--projects', nargs='+', help='specifies the projects', required=True)
 parser.add_argument('-r', '--reviewers', nargs='+', help='specifies the reviewers to add to the patches')
 parser.add_argument('-v', '--verbose', action='store_true', help='execute with extensive logging')
 parser.add_argument('-cr', '--code-review', dest='codeReview', help='code review to apply')
 parser.add_argument('-vr', '--verified', dest='verified', help='verified review to apply')
+parser.add_argument('-lb', '--local-branch', dest='localBranch', help='perform the operation on a local branch')
 parser.set_defaults(abandon=False)
+subparsers = parser.add_subparsers(help='sub-command help')
+
+gerrit_topic_parser = subparsers.add_parser('gerrit-topic', help='Gerrit topics')
+gerrit_topic_parser.add_argument('-t','--topics', nargs='+', help='topics to perform on', required=True)
+gerrit_topic_parser.add_argument('-o', '--owners', nargs='+', help='specifies the owners of the patches', required=True)
+gerrit_topic_parser.add_argument('-pr', '--projects', nargs='+', help='specifies the projects', required=True)
+gerrit_topic_parser.add_argument('-a', '--abandon',  dest='abandon', help='abandon the topic patches')
+gerrit_topic_parser.add_argument('-d', '--delete-drafts',  dest='deleteDrafts', help='delete draft changes')
+
+current_branch_parser = subparsers.add_parser('current-branch', help='Current branch')
+gerrit_topic_parser.add_argument('-p', '--path',  dest='path', help='path', required=True)
 
 args = parser.parse_args()
 d = vars(args)
@@ -70,11 +77,29 @@ reviewers = safeGet("reviewers", d)
 code_review = safeGet("codeReview", d)
 verified = safeGet("verified", d)
 
-for line in data:
-	parsed=json.loads(line)
-	print parsed
+def get_topic_commits(data):
+	result = []
+	for line in data:
+		parsed=json.loads(line)
+		commit=parsed["currentPatchSet"]["revision"]
+		result += [commit]
 
-	commit=parsed["currentPatchSet"]["revision"]
+	return result
+
+def get_branch_commits(data):
+	command = 'git log --format="%H"'
+	subprocess.run(["git log ", "-l", "/dev/null"], stdout=subprocess.PIPE)
+	result = []
+	for line in data:
+		parsed=json.loads(line)
+		commit=parsed["currentPatchSet"]["revision"]
+		result += [commit]
+
+	return result
+
+commits = get_topic_commits(data)
+
+for commit in commits:
 	if reviewers is not None:
 		reviewers_com = "set-reviewers {} --add ".format(commit)
 		reviewers_com+="--add ".join(reviewers)
